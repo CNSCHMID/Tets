daab4054c13ccd72c020dc9c4800336373749521
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2019-08-27 08:47:21 +0200

    Prepared first test

---
+++ Server/src/test/java/de/uol/swp/server/usermanagement/store/MainMemoryBasedUserStoreTest.java
@@ -0,0 +1,27 @@
+package de.uol.swp.server.usermanagement.store;
+
+import org.junit.jupiter.api.Test;
+
+class MainMemoryBasedUserStoreTest {
+
+    @Test
+    void findUser() {
+    }
+
+    @Test
+    void findUser1() {
+    }
+
+    @Test
+    void createUser() {
+
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+}
