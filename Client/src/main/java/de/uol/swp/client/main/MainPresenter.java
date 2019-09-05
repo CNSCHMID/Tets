c3204af0b6471ae335f65be31c57772543cc0649
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2019-09-05 13:27:25 +0200

    Do not call full user list each time a new user is logged in

--- Client/src/main/java/de/uol/swp/client/main/MainPresenter.java
+++ Client/src/main/java/de/uol/swp/client/main/MainPresenter.java
@@ -2,10 +2,12 @@ package de.uol.swp.client.main;
 
 import com.google.common.eventbus.Subscribe;
 import de.uol.swp.client.AbstractPresenter;
+import de.uol.swp.common.user.User;
 import de.uol.swp.common.user.dto.UserDTO;
 import de.uol.swp.common.user.message.UserLoggedInMessage;
 import de.uol.swp.common.user.message.UserLoggedOutMessage;
 import de.uol.swp.common.user.response.AllOnlineUsersResponse;
+import de.uol.swp.common.user.response.LoginSuccessfulMessage;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,31 +26,43 @@ public class MainPresenter extends AbstractPresenter {
 
     private ObservableList<String> users;
 
+    private User loggedInUser;
+
     @FXML
     private ListView<String> usersView;
 
     @Subscribe
-    public void newUser(UserLoggedInMessage message) {
-        LOG.debug("New user "+message.getUsername()+" logged in");
+    public void loginSucessful(LoginSuccessfulMessage message) {
+        this.loggedInUser = message.getUser();
         userService.retrieveAllUsers();
+    }
+
+    @Subscribe
+    public void newUser(UserLoggedInMessage message) {
+
+        LOG.debug("New user " + message.getUsername() + " logged in");
+        Platform.runLater(() -> {
+            if (users != null && loggedInUser != null && !loggedInUser.equals(message.getUsername()))
+                users.add(message.getUsername());
+        });
     }
 
     @Subscribe
     public void userLeft(UserLoggedOutMessage message) {
-        LOG.debug("User "+message.getUsername()+" logged out");
-        userService.retrieveAllUsers();
+        LOG.debug("User " + message.getUsername() + " logged out");
+        Platform.runLater(() -> users.remove(message.getUsername()));
     }
 
     @Subscribe
     public void userList(AllOnlineUsersResponse allUsersResponse) {
-        LOG.debug("Update of user list "+allUsersResponse.getUsers());
+        LOG.debug("Update of user list " + allUsersResponse.getUsers());
         updateUsersList(allUsersResponse.getUsers());
     }
 
     private void updateUsersList(List<UserDTO> userList) {
         // Attention: This must be done on the FX Thread!
         Platform.runLater(() -> {
-            if (users == null){
+            if (users == null) {
                 users = FXCollections.observableArrayList();
                 usersView.setItems(users);
             }
@@ -56,8 +70,6 @@ public class MainPresenter extends AbstractPresenter {
             userList.forEach(u -> users.add(u.getUsername()));
         });
     }
-
-
 
 
 }
