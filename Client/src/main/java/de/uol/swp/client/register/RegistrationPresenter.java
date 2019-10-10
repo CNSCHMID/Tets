450ec92223cb4ab3bb86a7314e930c1bc086903a
Marco Grawunder <Marco.Grawunder@uni-oldenburg.de>
2019-10-10 16:32:02 +0200

    RegistrationEventTest

--- Client/src/main/java/de/uol/swp/client/register/RegistrationPresenter.java
+++ Client/src/main/java/de/uol/swp/client/register/RegistrationPresenter.java
@@ -4,8 +4,8 @@ import com.google.common.base.Strings;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
 import de.uol.swp.client.AbstractPresenter;
-import de.uol.swp.client.register.event.RegistationErrorEvent;
 import de.uol.swp.client.register.event.RegistrationCanceledEvent;
+import de.uol.swp.client.register.event.RegistrationErrorEvent;
 import de.uol.swp.common.user.UserService;
 import de.uol.swp.common.user.dto.UserDTO;
 import javafx.event.ActionEvent;
@@ -44,11 +44,11 @@ public class RegistrationPresenter extends AbstractPresenter {
     @FXML
     void onRegisterButtonPressed(ActionEvent event) {
         if (Strings.isNullOrEmpty(loginField.getText())){
-            eventBus.post(new RegistationErrorEvent("Username cannot be empty"));
+            eventBus.post(new RegistrationErrorEvent("Username cannot be empty"));
         }else if(!passwordField1.getText().equals(passwordField2.getText())) {
-            eventBus.post(new RegistationErrorEvent("Passwords are not equal"));
+            eventBus.post(new RegistrationErrorEvent("Passwords are not equal"));
         } else if (Strings.isNullOrEmpty(passwordField1.getText())) {
-            eventBus.post(new RegistationErrorEvent("Password cannot be empty"));
+            eventBus.post(new RegistrationErrorEvent("Password cannot be empty"));
         } else {
             userService.createUser(new UserDTO(loginField.getText(), passwordField1.getText(), "empty"));
         }
